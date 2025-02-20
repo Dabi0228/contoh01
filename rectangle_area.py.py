c478a7e421dda7095ecb68b16cb0ce3769772731
Dabi0228 <chestarannga@gmail.com>
2025-02-20 16:22:56 +0700

    updatee

--- rectangle_area.py.py
+++ rectangle_area.py.py
@@ -8,9 +8,6 @@ width = 10
 area = calculate_area(length, width)
 print(f"Luas persegi panjang adalah: {area}")
 #selesaiiiiii
-length = 5
-width = 10
-area = calculate_area(length, width)
 perimeter = calculate_perimeter(length, width)
 
 print(f"Luas persegi panjang adalah: {area}")  # Dari branch Maya
