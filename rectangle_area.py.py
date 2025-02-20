5ce183dddd0ab2235fc7b5ecdac924850839d7e7
Dabi0228 <chestarannga@gmail.com>
2025-02-20 15:45:48 +0700

    update

--- rectangle_area.py.py
+++ rectangle_area.py.py
@@ -8,19 +8,8 @@ width = 10
 area = calculate_area(length, width)
 print(f"Luas persegi panjang adalah: {area}")
 #selesaiiiiii
-length = 5
-width = 10
-area = calculate_area(length, width)
 perimeter = calculate_perimeter(length, width)
 
-print(f"The area of the rectangle is: {area}")  # Dari branch Maya
-print(f"The perimeter of the rectangle is: {perimeter}")  # Dari branch Maya
-length = 5
-width = 10
-area = calculate_area(length, width)
-perimeter = calculate_perimeter(length, width)
-
-print(f"Luas persegi panjang adalah: {area}")  # Dari branch utama
 print(f"The area of the rectangle is: {area}")  # Dari branch Maya
 print(f"The perimeter of the rectangle is: {perimeter}")  # Dari branch Maya
 #selesai
