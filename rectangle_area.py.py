d84b53e01efc011a57124a1ffafe61999278af62
Dabi0228 <chestarannga@gmail.com>
2025-02-20 14:44:18 +0700

    Menambahkan output hasil luas dan keliling

--- rectangle_area.py.py
+++ rectangle_area.py.py
@@ -8,3 +8,10 @@ width = 10
 area = calculate_area(length, width)
 print(f"Luas persegi panjang adalah: {area}")
 #selesaiiiiii
+length = 5
+width = 10
+area = calculate_area(length, width)
+perimeter = calculate_perimeter(length, width)
+
+print(f"The area of the rectangle is: {area}")  # Dari branch Maya
+print(f"The perimeter of the rectangle is: {perimeter}")  # Dari branch Maya
