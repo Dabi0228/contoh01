fcd824dd1cfd4ee66d7e5c2cfceeaf0b492d37c4
Dabi0228 <chestarannga@gmail.com>
2025-02-20 13:25:58 +0700

    Menambahkan file rectangle_area.py

---
+++ rectangle_area.py.py
@@ -0,0 +1,9 @@
+def calculate_area(length, width):
+    """Menghitung luas persegi panjang."""
+    return length * width  # Perkalian yang benar
+
+# Contoh penggunaan
+length = 5
+width = 10
+area = calculate_area(length, width)
+print(f"Luas persegi panjang adalah: {area}")
